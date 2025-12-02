59e91418fe93fb397aff2122b0f56e4f4c68e5d7
qqqkyj <123rkdduswn@naver.com>
2025-12-02 11:26:04 +0900

    í회원가입 화면 êµã생성

--- springboot/instagram/src/main/java/com/example/instagram/controller/AuthController.java
+++ springboot/instagram/src/main/java/com/example/instagram/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.example.instagram.controller;
 
+import com.example.instagram.dto.request.SignUpRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,5 +15,12 @@ public class AuthController {
     @GetMapping("/login")
     public String login(){
         return "auth/login";
+    }
+
+    //회원가입 화면
+    @GetMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("signUpRequest", new SignUpRequest());
+        return "auth/signup";
     }
 }
