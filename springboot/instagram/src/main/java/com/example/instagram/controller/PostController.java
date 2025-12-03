2230299fda7d6c5bcdb62e9c28af5008f20a11b9
qqqkyj <123rkdduswn@naver.com>
2025-12-03 17:44:45 +0900

    댓글 폼 생성

--- springboot/instagram/src/main/java/com/example/instagram/controller/PostController.java
+++ springboot/instagram/src/main/java/com/example/instagram/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.example.instagram.controller;
 
+import com.example.instagram.dto.request.CommentCreateRequest;
 import com.example.instagram.dto.request.PostCreateRequest;
 import com.example.instagram.dto.response.PostResponse;
 import com.example.instagram.security.CustomUserDetails;
@@ -42,9 +43,10 @@ public class PostController {
     @GetMapping("{id}")
     public String detail(@PathVariable Long id, Model  model,
                          @AuthenticationPrincipal CustomUserDetails userDetails) {
-        PostResponse post = postService.getPostById(id);
+        PostResponse post = postService.getPostById(id);//게시물 전체 조회
 
         model.addAttribute("post",post);
+        model.addAttribute("commentRequest", new CommentCreateRequest());
         return "post/detail";
     }
 }
---
+++ springboot/instagram/src/main/java/com/example/instagram/dto/request/CommentCreateRequest.java
@@ -0,0 +1,13 @@
+package com.example.instagram.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CommentCreateRequest {
+    @NotBlank
+    @Size(max = 500, message = "댓글은 500자 이내로 작성해 주세요")
+    private String content;
+}
